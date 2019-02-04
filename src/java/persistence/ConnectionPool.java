267c5a8ac2fcdc2f78c0bc271e7f53983976bd09
756852 <756852@ELF7IT219020.ACDM.DS.SAIT.CA>
2019-02-04 01:04:06 -0700

    Connection pool made. No comments yet

---
+++ src/java/persistence/ConnectionPool.java
@@ -0,0 +1,57 @@
+package persistence;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class ConnectionPool 
+{
+    private static ConnectionPool pool = null;
+    private static DataSource dataSource = null;
+
+    private ConnectionPool()
+    {
+        try
+        {
+            InitialContext ic = new InitialContext();            
+        } catch (NamingException ex)
+        {
+            System.out.println(ex);
+        }
+    }
+    
+    public static synchronized ConnectionPool getInstance()
+    {
+        if(pool == null)
+        {
+            pool = new ConnectionPool();
+        }
+        return pool;
+    }
+    
+    public Connection getConnection()
+    {
+        try 
+        {
+            return dataSource.getConnection();
+        } catch (SQLException ex) 
+        {
+            System.out.println(ex);
+            return null;
+        }
+    }
+    
+    public void releaseConnection(Connection conn)
+    {
+        try
+        {
+            conn.close();
+        }catch(SQLException e)
+        {
+            System.out.println();
+        }
+    }
+}
