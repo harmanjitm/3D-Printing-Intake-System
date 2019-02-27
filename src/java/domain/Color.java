76473a021018ae3962d13731d8315d97746a6ea0
672762 <672762@elf7it219918.ACDM.DS.SAIT.CA>
2019-02-27 14:49:31 -0700

    Merge origin/master
    Added a Color object

---
+++ src/java/domain/Color.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author Ben Wozak
+ */
+public class Color {
+    
+    private String color;
+    private String status;
+
+    /**
+     * Initiates a new Color.
+     */
+    public Color() {
+    }
+
+    /**
+     * Initiates a new Color.
+     * @param color the color
+     * @param status the color
+     */
+    public Color(String color, String status) {
+        this.color = color;
+        this.status = status;
+    }
+
+    /**
+     * Gets the color of a color object
+     * @return 
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * Sets the color of a color object
+     * @param color 
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * Gets the status of a color object
+     * @return status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * Sets the status of a color object
+     * @param status 
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+}
