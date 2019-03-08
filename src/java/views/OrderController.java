53bb573084bc344bacbb52594e84501144abd098
687159 <687159@elf7it216372.ACDM.DS.SAIT.CA>
2019-03-08 10:05:24 -0700

    Haseeb: Started work on QueueManagement and OrderController

--- src/java/views/OrderController.java
+++ src/java/views/OrderController.java
@@ -21,7 +21,7 @@ public class OrderController extends HttpServlet
 {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
     {
-        
+        request.getRequestDispatcher("/WEB-INF/newOrder.jsp").forward(request, response);
     }
 
     @Override
