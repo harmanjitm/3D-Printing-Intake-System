f3a1477eacd3d1b5819349bc16dac4277d4a93b4
758243 <758243@169.254.2.140>
2019-03-27 14:07:39 -0600

    3D Printing Intake System Added reporting dependencies.

--- src/java/services/ReportService.java
+++ src/java/services/ReportService.java
@@ -3,7 +3,13 @@ package services;
 import java.util.ArrayList;
 
 import domain.Report;
+import java.io.IOException;
 import java.sql.SQLException;
+import org.apache.fontbox.FontBoxFont;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import persistence.ReportBroker;
 
 /**
@@ -22,7 +28,22 @@ public class ReportService {
         return rb.getAllReports();
     }
 
-    public void createReport() {
+    public void createReport() throws IOException {
+        PDDocument report = new PDDocument();
+        PDPage page = new PDPage();
 
+        report.addPage(page);
+
+        PDPageContentStream cs = new PDPageContentStream(report, page);
+        
+        cs.beginText();//Begin writing
+        
+        cs.newLineAtOffset(25, 700);
+        cs.setFont(PDType1Font.COURIER_BOLD, 20);
+        cs.showText("ARIS3D Report");
+        cs.endText();//End writing
+        
+        report.save("C:/PDF/test.pdf");
+        report.close();
     }
 }
