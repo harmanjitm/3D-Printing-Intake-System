cdfa5a3db13d2445a8d4463855a027c0604acfdf
687159 <687159@elf7it216372.ACDM.DS.SAIT.CA>
2019-04-03 14:47:50 -0600

    Haseeb: there stfu

--- src/java/views/OrderController.java
+++ src/java/views/OrderController.java
@@ -27,6 +27,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -36,6 +38,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.apache.tomcat.util.http.fileupload.RequestContext;
 import services.AccountService;
 import services.MaterialService;
 import services.OrderService;
@@ -89,8 +96,6 @@ public class OrderController extends HttpServlet
         /**
          * order submit needs to take in:
          *          cost
-         *          orderDate
-         *          PrintDate
          *          Status
          *          file object
          *          printer object
@@ -98,26 +103,17 @@ public class OrderController extends HttpServlet
          *          account object     
          *          comments,    
          */
-        
+        int maxFileSize = 50 * 1024;
+        int maxMemSize = 4 * 1024;
+        File file;
+        String filePath = "C\\STL";
         String action = request.getParameter("action");
         String orderIDs = request.getParameter("orderID");
         String costS = request.getParameter("cost");
         String printerIDs = request.getParameter("printerID");
         String materialIDs = request.getParameter("materialID");
         String status = request.getParameter("status"); //dont think you need
-        String payment = request.getParameter("payment"); //dont think you need this
-        
-        
-        Part filePart = request.getPart("file");
-        String name = filePart.getSubmittedFileName();
-        Path path = Paths.get(name);
-        String fileName = path.getFileName().toString();
-        
-        InputStream fileContent = filePart.getInputStream();
-        
-        String storagePath = request.getServletContext().getRealPath("");
-        String uploadFilePath = storagePath + File.separator + fileName;
-        
+        String payment = request.getParameter("payment"); //dont think you need this        
         
         Printer printer = new Printer();
         Material material = new Material();
@@ -128,7 +124,7 @@ public class OrderController extends HttpServlet
         PrinterService ps = new PrinterService();
         MaterialService ms = new MaterialService();
         
-        int orderID = Integer.parseInt(orderIDs);
+        //int orderID = Integer.parseInt(orderIDs);
         double cost = Double.parseDouble(costS);
         
         switch(action)
@@ -141,19 +137,50 @@ public class OrderController extends HttpServlet
                     cost = printer.getRunCost();
                     status = printer.getStatus();
 
-                    InputStream initialStream = new FileInputStream(
-                    new File("file"));
-                    byte[] buffer = new byte[initialStream.available()];
-                    initialStream.read(buffer);
+                    Part filePart = request.getPart("file");
+                    String name = filePart.getSubmittedFileName();
 
-                    File targetFile = new File(uploadFilePath);
-                    OutputStream outStream = new FileOutputStream(targetFile);
-                    outStream.write(buffer);
+                    DiskFileItemFactory factory = new DiskFileItemFactory();
+                    factory.setSizeThreshold(maxMemSize);
+                    factory.setRepository(new File("c:\\temp"));
+                    ServletFileUpload upload = new ServletFileUpload(factory);
+                    upload.setSizeMax(maxFileSize);
+                    try
+                    {
+                        List fileItems = upload.parseRequest((RequestContext) request);
+                        Iterator i = fileItems.iterator();
+                        while ( i.hasNext () ) 
+                        {
+                            FileItem fi = (FileItem)i.next();
+                            if ( !fi.isFormField () ) 
+                            {
+                                // Get the uploaded file parameters
+                                String fieldName = fi.getFieldName();
+                                String fileName = fi.getName();
+                                
+                                String contentType = fi.getContentType();
+                                boolean isInMemory = fi.isInMemory();
+                                long sizeInBytes = fi.getSize();
+                                if( fileName.lastIndexOf("\\") >= 0 ) 
+                                {
+                                    file = new File( filePath + fileName.substring( fileName.lastIndexOf("\\"))) ;
+                                } 
+                                else 
+                                {
+                                    file = new File( filePath + fileName.substring(fileName.lastIndexOf("\\")+1)) ;
+                                }
+                                fi.write(file) ;
+                            }
+                        }
+                    } 
+                    catch(Exception ex) 
+                    {
+                        Logger.getLogger(OrderController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                     
                     printer = ps.getPrinterById(printerID);
                     material = ms.getMaterial(materialID);
-                    orderID = order.getOrderId();
-                    order = os.createOrder(orderID, cost, status, targetFile, payment, printer, material);
+//                    order = os.createOrder(orderID, cost, status, file, payment, printer, material);
                     
                     request.setAttribute("successMessage", "Order has been submitted");
                     request.getRequestDispatcher("/WEB-INF/newOrder.jsp").forward(request, response); 
@@ -167,6 +194,37 @@ public class OrderController extends HttpServlet
                 request.setAttribute("errorMessage", "Please follow the steps to submit order");
                 request.getRequestDispatcher("/WEB-INF/newOrder.jsp").forward(request, response);
                 break;
+            case "download":
+                try {
+                    String path = request.getParameter("C:\\Users\\687159\\Desktop\\18.stl");
+                    File downloadFile = new File(path);
+                    FileInputStream inStream = new FileInputStream(downloadFile);
+                    String mimeType = getServletContext().getMimeType(path);
+
+                    response.setContentType(mimeType);
+                    response.setContentLength((int) downloadFile.length());
+
+                    String headerKey = "Content-Disposition";
+                    String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
+                    response.setHeader(headerKey, headerValue);
+
+                    OutputStream outStream = response.getOutputStream();
+
+                    byte[] buffer = new byte[4096];
+                    int bytesRead = -1;
+
+                    while ((bytesRead = inStream.read(buffer)) != -1) {
+                        outStream.write(buffer, 0, bytesRead);
+                    }
+
+                    inStream.close();
+                    outStream.close();
+                    return;
+                } catch (Exception e) {
+                    request.setAttribute("errorMessage", "Error Downloading Report: File might not exist or path is incorrect.");
+                    request.getRequestDispatcher("/WEB-INF/reportMgmt.jsp").forward(request, response);
+                    return;
+                }
         }
       
     }
