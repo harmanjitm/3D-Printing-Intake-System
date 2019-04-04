5ef1c0314a3b3f17771101cd988f2ea15fab75ca
687159 <687159@elf7it216372.ACDM.DS.SAIT.CA>
2019-04-04 12:02:16 -0600

    Haseeb: switches and shit on queue controller and shit

--- src/java/views/QueueManagementController.java
+++ src/java/views/QueueManagementController.java
@@ -11,7 +11,10 @@ import domain.Material;
 import domain.Order;
 import domain.OrderQueue;
 import domain.Printer;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -138,6 +141,40 @@ public class QueueManagementController extends HttpServlet
                         request.getRequestDispatcher("/WEB-INF/queueMgmt.jsp").forward(request, response);
                     }
                     break;
+                case "download":
+                    try 
+                    {
+                        String path = request.getParameter("path");
+                        File downloadFile = new File(path);
+                        FileInputStream inStream = new FileInputStream(downloadFile);
+                        String mimeType = getServletContext().getMimeType(path);
+
+                        response.setContentType(mimeType);
+                        response.setContentLength((int) downloadFile.length());
+
+                        String headerKey = "Content-Disposition";
+                        String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
+                        response.setHeader(headerKey, headerValue);
+
+                        OutputStream outStream = response.getOutputStream();
+
+                        byte[] buffer = new byte[4096];
+                        int bytesRead = -1;
+
+                        while ((bytesRead = inStream.read(buffer)) != -1) {
+                            outStream.write(buffer, 0, bytesRead);
+                        }
+
+                        inStream.close();
+                        outStream.close();
+                        return;
+                    } 
+                    catch(Exception e)
+                    {
+                        request.setAttribute("errorMessage", "Error Downloading File: File might not exist or path is incorrect.");
+                        request.getRequestDispatcher("/WEB-INF/queueMgmt.jsp").forward(request, response);
+                        return;
+                    }
             }
         } 
         catch (SQLException ex)
