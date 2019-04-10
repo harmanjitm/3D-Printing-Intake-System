415ed4e7abf9685a82f49896b47710596681f5c3
756852 <756852@ELF7IT219020.ACDM.DS.SAIT.CA>
2019-04-10 14:06:24 -0600

    BackupBroker done!

--- src/java/persistence/BackupBroker.java
+++ src/java/persistence/BackupBroker.java
@@ -1,74 +1,48 @@
 package persistence;
 
-import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-
-public class BackupBroker implements Broker{
-	
-
-	/**
-	 * Instantiates a new Backup broker.
-	 */
-	private BackupBroker() {
-		
-	}
-	
-	/**
-	 * Gets the broker.
-	 *
-	 * @return the broker
-	 */
-	public BackupBroker getBroker() {
-		return null;
-		
-	}
-	
-	/* (non-Javadoc)
-	 * @see persistence.Broker#closeBroker()
-	 */
-	public void closeBroker() {
-		
-	}
-
-	/* (non-Javadoc)
-	 * @see persistence.Broker#search(int)
-	 */
-	@Override
-	public Object search(int id) {
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see persistence.Broker#persist(java.lang.Object)
-	 */
-	@Override
-	public boolean persist(Object object) {
-		return false;
-	}
-
-	/* (non-Javadoc)
-	 * @see persistence.Broker#remove(java.lang.Object)
-	 */
-	@Override
-	public boolean remove(Object object) {
-		return false;
-	}
-
-	/* (non-Javadoc)
-	 * @see persistence.Broker#execute(java.lang.String)
-	 */
-	@Override
-	public ResultSet execute(String statement) {
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see persistence.Broker#getAll()
-	 */
-	@Override
-	public ArrayList<Object> getAll() {
-		return null;
-	}
-
+public class BackupBroker{
+    
+    /**
+     * Creates a backup of the database
+     * @return Returns a 1 if the backup was successful, 0 if unsuccessful
+     * @throws IOException, InterruptedException
+     */
+    public int createBackup() throws IOException, InterruptedException{        
+        String mysqlDumpLocation = "C:/Program Files/MySQL/MySQL Server 5.7/bin/mysqldump.exe";
+        String outputDir = "C:/Backups/";
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String finalOutput = outputDir+date+".sql";
+        
+        Runtime rt = Runtime.getRuntime();
+        Process p = rt.exec(mysqlDumpLocation+" -uroot -ppassword --add-drop-database -B aris -r"+finalOutput);
+        
+        int processComplete = p.waitFor();
+        
+        if (processComplete == 0){
+            return 1;
+        }
+        else return 0;
+    }
+    
+    /**
+     * Restores database from an old backup
+     * @param filename Name of the backup file to be restored, without the .sql extension
+     * @return Returns 1 if successful, 0 if unsuccessful
+     * @throws IOException 
+     */
+    public int restoreBackup(String filename) throws IOException, InterruptedException{
+        
+        Process p = Runtime.getRuntime().exec("cmd /c \"C:/Program Files/MySQL/MySQL Server 5.7/bin/mysql.exe\" -uroot -ppassword aris < C:/Backups/"+filename+".sql");
+        
+        int processComplete = p.waitFor();
+        
+        if (processComplete == 0){
+            return 1;
+        }
+        else return 0;
+    }
 }
